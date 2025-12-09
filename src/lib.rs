7b0c6b28b20884226b6329e17e7eaca77eb15c89
Nockyx <95299389+Nockyx@users.noreply.github.com>
2025-12-08 21:29:40 -0300

    debug macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,24 @@
 pub mod aocutils {
 
+    #[macro_export]
+    macro_rules! dbgp {
+
+    () => {
+        eprintln!("[{}:{}:{}]", file!(), line!(), column!())
+    };
+    ($val:expr $(,)?) => {
+
+        match $val {
+            tmp => {
+                eprintln!("[{}:{}:{}] {} = {:?}",
+                    file!(), line!(), column!(), stringify!($val), &tmp);
+                tmp
+            }
+        }
+    };
+    ($($val:expr),+ $(,)?) => {
+        ($($crate::dbgp!($val)),+,)
+    };
+}
+    
 }
